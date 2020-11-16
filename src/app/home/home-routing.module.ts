ec645d801bf661da5397d553e22ab886466b2e07
myflowpl <pedro.blaszczak@gmail.com>
2020-11-16 16:09:21 +0100

    home page

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePage } from './pages/home/home.page';
 
 const routes: Routes = [
   {
-    
-  }
+    path: '',
+    component: HomePage,
+  },
+  // {
+  //   path: '/contact',
+  //   component: ContactPage,
+  // }
 ];
 
 @NgModule({
