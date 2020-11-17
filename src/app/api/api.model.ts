c3623a099095ad273c700f0c6ac3f3fde6546f50
myflowpl <pedro.blaszczak@gmail.com>
2020-11-17 08:43:03 +0100

    api server

---
+++ src/app/api/api.model.ts
@@ -0,0 +1,45 @@
+export interface Coords {
+  lat: number;
+  lng: number;
+}
+export interface Artist {
+  id: number;
+  name: string;
+  img: string;
+  location?: Coords;
+}
+export interface Playlist {
+  id: number;
+  name: string;
+}
+
+export interface PlaylistSongs {
+  id: number;
+  playlistId: number;
+  songId: number;
+  song?: Song;
+  playlist?: Playlist;
+}
+
+export interface Song {
+  id: number;
+  title: string;
+  year: string;
+  artistId: number;
+  webUrl: string;
+  genders?: string[];
+  favorite?: boolean;
+}
+
+export interface User {
+  id: number;
+  name: string;
+  password: string;
+  role?: Role;
+}
+
+export enum Role {
+  USER = 'user',
+  ADMIN = 'admin',
+  ROOT = 'root',
+}
