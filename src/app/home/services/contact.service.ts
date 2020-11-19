ac62f9e0ef4ba2f2ac4ab05ad8f48d92e57e2a07
myflowpl <pedro.blaszczak@gmail.com>
2020-11-19 10:02:38 +0100

    update

--- src/app/home/services/contact.service.ts
+++ src/app/home/services/contact.service.ts
@@ -1,18 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
 import { Observable, Subject, from, BehaviorSubject, merge } from 'rxjs';
-import { startWith, switchMap, tap, take } from 'rxjs/operators';
+import { startWith, switchMap, tap, take, filter } from 'rxjs/operators';
 import { Contact } from 'src/app/api/api.model';
 import { API_BASE_URL } from '../../api/api.tokens';
 
-function cache<R = any, T = any>(cache$: BehaviorSubject<R>, requestFn: (val: T, i: number) => Observable<R>) {
+function cache<R = any, T = any>(cache$: BehaviorSubject<R>, requestFn: (val: T) => Observable<R>) {
   return (in$: Observable<T>) => {
     return merge(
-      cache$.pipe(take(1)),
+      cache$,
       in$.pipe(
         startWith(1 as any),
         switchMap(requestFn),
         tap(data => cache$.next(data)),
+        filter(() => false),
       )
     )
   }
@@ -34,7 +35,7 @@ export class ContactService {
 
   getContacts(): Observable<Contact[]> {
     return this.reload$.pipe(
-      cache(this.contacts$, () => this.http.get<Contact[]>(this.base + '/contacts')),
+      cache(this.contacts$, (v) => this.http.get<Contact[]>(this.base + '/contacts')),
     );
   }
 
@@ -44,13 +45,13 @@ export class ContactService {
 
   delete(id: number): Observable<void> {
     return this.http.delete<void>(this.base + '/contacts/'+id).pipe(
-      tap(() => this.reload$.next())
+      tap(() => this.reload$.next(true))
     );
   }
 
   create(contact: Partial<Contact>): Observable<Contact> {
     return this.http.post<Contact>(this.base + '/contacts', contact).pipe(
-      tap(() => this.reload$.next())
+      tap(() => this.reload$.next(true))
     );
   }
 }
