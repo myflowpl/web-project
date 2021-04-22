dc26be95ee10c6992a256e603842a84fc97f567b
myflowpl <pedro.blaszczak@gmail.com>
2021-04-22 11:37:31 +0200

    contact details

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactPage } from './pages/contact/contact.page';
 import { HomePage } from './pages/home/home.page';
+import { ContactDetailsComponent } from './components/contact-details/contact-details.component';
 
 @NgModule({
   declarations: [
     HomePage,
     ContactPage,
+    ContactDetailsComponent,
   ],
   imports: [
     CommonModule,
--- src/app/home/pages/contact/contact.page.ts
+++ src/app/home/pages/contact/contact.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Contact } from '../../../api/api.models';
 
 @Component({
   selector: 'app-contact',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.page.scss']
 })
 export class ContactPage implements OnInit {
+
+  public contacts: Contact[] = [];
 
   constructor() { }
 
