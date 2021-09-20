d9670e6fa44c6877f05ada6a2bc9b5d4cf3ab1b5
myflowpl <pedro.blaszczak@gmail.com>
2021-09-20 16:09:46 +0200

    view encapsulation

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   template: `
     <p>
-      footer works!
+      <a href="https://myflow.pl">My Flow Studio</a>
     </p>
   `,
   styles: [`
-
-  `]
+  a {
+    display: block;
+    text-align: center;
+    padding: 5px;
+    border-top: 1px solid gray;
+    color: green;
+  }
+  `],
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class FooterComponent implements OnInit {
 
