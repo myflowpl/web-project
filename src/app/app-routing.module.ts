3af761baf9c71d5d4a58da4b79d6ea686e9607ea
myflowpl <pedro.blaszczak@gmail.com>
2021-09-20 15:39:57 +0200

    routing setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactPage } from './home/pages/contact/contact.page';
+import { HomePage } from './home/pages/home/home.page';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePage
+  },
+  {
+    path: 'contact',
+    component: ContactPage
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HomePage } from './home/pages/home/home.page';
+import { ContactPage } from './home/pages/contact/contact.page';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomePage,
+    ContactPage
   ],
   imports: [
     BrowserModule,
