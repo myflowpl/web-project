ffe2c9dc823c459dfb85dff7a3947be1b0548f29
myflowpl <pedro.blaszczak@gmail.com>
2021-09-22 09:56:54 +0200

    contact service reload data

--- src/app/home/services/contact.service.ts
+++ src/app/home/services/contact.service.ts
@@ -1,26 +1,37 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { Contact } from '../../api/api.models';
+import { switchMap, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
 
+  private reload$ = new BehaviorSubject<Contact | undefined>(undefined);
+
   constructor(
     private http: HttpClient,
   ) { }
 
   getById(id: number): Observable<Contact> {
-    return this.http.get<Contact>(`http://localhost:3000/contacts/${id}`);
+    return this.reload$.pipe(
+      switchMap(() => this.http.get<Contact>(`http://localhost:3000/contacts/${id}`))
+    );
   }
 
   getContacts(): Observable<Contact[]> {
-    return this.http.get<Contact[]>(`http://localhost:3000/contacts`);
+
+    return this.reload$.pipe(
+      switchMap(() => this.http.get<Contact[]>(`http://localhost:3000/contacts`)),
+    );
+
   }
 
   update(contact: Contact): Observable<Contact> {
-    return this.http.patch<Contact>(`http://localhost:3000/contacts/${contact.id}`, contact);
+    return this.http.patch<Contact>(`http://localhost:3000/contacts/${contact.id}`, contact).pipe(
+      tap((contact) => this.reload$.next(contact))
+    );
   }
 }
