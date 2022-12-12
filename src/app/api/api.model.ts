d9b8c86d7067984d31bb125bb6564341b834b67a
myflowpl <pedro.blaszczak@gmail.com>
2022-12-12 08:45:05 +0100

    update api scripts

---
+++ src/app/api/api.model.ts
@@ -0,0 +1,61 @@
+
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+  password?: string;
+  role?: Role
+}
+
+export enum Role {
+    USER = 'user',
+    ADMIN = 'admin',
+    ROOT = 'root',
+}
+
+export interface UserCreateResponse {
+  accessToken: string;
+  user: User;
+}
+
+export interface Contact {
+  id: number;
+  name: string;
+  email: string;
+}
+
+export interface Coords {
+  lat: number;
+  lng: number;
+}
+export interface Artist {
+  id: number;
+  name: string;
+  img: string;
+  location?: Coords;
+}
+export interface Playlist {
+  id: number;
+  name: string;
+  userId: number;
+}
+
+export interface PlaylistSongs {
+  id: number;
+  playlistId: number;
+  songId: number;
+  song?: Song;
+  playlist?: Playlist;
+  userId: number;
+}
+
+export interface Song {
+  id: number;
+  title: string;
+  year: string;
+  artistId: number;
+  artist?: Artist;
+  webUrl: string;
+  genders?: string[];
+  favorite?: boolean;
+}
