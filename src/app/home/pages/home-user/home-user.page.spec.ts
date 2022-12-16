214132de0a297d67778ee21e52c5b27d2adf7a6f
myflowpl <pedro.blaszczak@gmail.com>
2022-12-16 14:19:21 +0100

    home users componetn

--- src/app/home/pages/home-user/home-user.page.spec.ts
+++ src/app/home/pages/home-user/home-user.page.spec.ts
@@ -1,22 +1,47 @@
+import { HttpClient } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { PetApi } from 'api-client';
+import { of } from 'rxjs';
+import { symbolName } from 'typescript';
 
 import { HomeUserPage } from './home-user.page';
 
-describe('HomeUserPage', () => {
+fdescribe('HomeUserPage', () => {
   let component: HomeUserPage;
   let fixture: ComponentFixture<HomeUserPage>;
+  let api: PetApi;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [HomeUserPage],
+      providers: [
+        {
+          provide: HttpClient,
+          useValue: {},
+        }
+      ],
     }).compileComponents();
 
+    // mock
+    api = TestBed.inject(PetApi);
+
+    const pets$ = of([
+      {id: 1, name: 'fluffy'},
+    ]);
+
+    spyOn(api, 'findPetsByStatus').and.returnValue(pets$ as any);
+
+    // create component
     fixture = TestBed.createComponent(HomeUserPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    expect(api.findPetsByStatus).toHaveBeenCalledTimes(1)
+    const text = fixture.nativeElement.querySelector('pre').textContent;
+    expect(text).toContain('fluffy')
   });
 });
