4bd4f97c95966d83afde297a41396d399c608bbd
myflowpl <pedro.blaszczak@gmail.com>
2023-02-20 10:00:07 +0100

    init layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainDirective } from './directives/main.directive';
+import { IfDirective } from './directives/if.directive';
+import { HomePage } from './pages/home/home.page';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    MainDirective,
+    IfDirective,
+    HomePage
   ],
   imports: [
     BrowserModule,
