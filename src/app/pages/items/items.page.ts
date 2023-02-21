1799ff4e6bed7e6b9ef6a282b79ccced49ae14ab
myflowpl <pedro.blaszczak@gmail.com>
2023-02-21 11:01:33 +0100

    lazy load & init module

--- src/app/pages/items/items.page.ts
+++ src/app/pages/items/items.page.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, inject, OnInit, TemplateRef, ViewChild, ViewContainerRef } from '@angular/core';
+import { AfterViewInit, Component, createNgModule, inject, Injector, OnInit, TemplateRef, ViewChild, ViewContainerRef } from '@angular/core';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
 
 export enum ItemType {
@@ -43,11 +43,14 @@ export class ItemsPage implements OnInit, AfterViewInit {
 
   ngAfterViewInit(): void {
     console.log('OUTLET after view INIT', this.itemOutlet, this.errorMessage)
+
   }
 
   ngOnInit(): void {
     console.log('OUTLET ON INIT', this.itemOutlet, this.errorMessage)
   }
+
+  injector = inject(Injector);
 
   async onSubmit() {
     console.log('is valid', this.form.valid)
@@ -60,6 +63,8 @@ export class ItemsPage implements OnInit, AfterViewInit {
       if(this.form.value.type === ItemType.MAP) {
         const { MapComponent } = await import('./types/map/map.component');
         comp = MapComponent;
+        // const ngModuleRef = createNgModule(LazyModule, this.injector);
+
       } else {
         const { TextComponent } = await import('./types/text/text.component');
         comp = TextComponent;
