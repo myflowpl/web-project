82508b143aa6d7adbeea445f876e74248ad2eaf0
myflowpl <pedro.blaszczak@gmail.com>
2024-06-03 15:24:38 +0200

    router

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
   imports: [],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrl: './footer.component.scss',
+  // encapsulation: ViewEncapsulation.None
 })
 export class FooterComponent {
 
