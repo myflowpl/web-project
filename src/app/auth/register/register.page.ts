bc8ac22d46ba974b752e5f9f55ccee30afac674c
myflowpl <pedro.blaszczak@gmail.com>
2024-06-05 11:02:48 +0200

    register form

--- src/app/auth/register/register.page.ts
+++ src/app/auth/register/register.page.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthStore } from '../auth.store';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './register.page.html',
   styleUrl: './register.page.scss'
 })
 export class RegisterPage {
+
+  fb = inject(NonNullableFormBuilder);
+  store = inject(AuthStore);
+
+  form = this.fb.group({
+    email: ['piotr@myflow.pl', [Validators.required, Validators.email], []],
+    password: ['!@#$', [Validators.minLength(3), Validators.required]],
+  });
+
+  onSubmit() {
+    console.log(this.form.value);
+  }
 
 }
