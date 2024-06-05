bfd28f971f7bc65850ecd36cf4b4fce64ebc4d29
myflowpl <pedro.blaszczak@gmail.com>
2024-06-05 16:35:39 +0200

    has role with params

--- src/app/directives/has-role.directive.ts
+++ src/app/directives/has-role.directive.ts
@@ -1,4 +1,6 @@
-import { Directive } from '@angular/core';
+import { Directive, TemplateRef, ViewContainerRef, effect, inject, input } from '@angular/core';
+import { AuthStore } from '../auth/auth.store';
+import { Role } from '../api/api.model';
 
 @Directive({
   selector: '[appHasRole]',
@@ -6,6 +8,27 @@ import { Directive } from '@angular/core';
 })
 export class HasRoleDirective {
 
-  constructor() { }
+  auth = inject(AuthStore);
+  templateRef = inject(TemplateRef);
+  viewContainerRef = inject(ViewContainerRef);
 
+  appHasRole = input<Role | string>()
+  roleConfig = input<any>()
+
+  constructor() {
+
+    effect(() => {
+
+      const user = this.auth.user();
+      const role = this.appHasRole();
+
+      if(user && (user.role === role)) {
+        this.viewContainerRef.createEmbeddedView(this.templateRef);
+      } else {
+        this.viewContainerRef.clear();
+      }
+
+    });
+    
+  }
 }
