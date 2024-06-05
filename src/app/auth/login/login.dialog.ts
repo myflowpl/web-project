144261dac02fe898506eebcbaf756172dfe90fbe
myflowpl <pedro.blaszczak@gmail.com>
2024-06-05 15:24:50 +0200

    login dialog guards

--- src/app/auth/login/login.dialog.ts
+++ src/app/auth/login/login.dialog.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { LoginFormComponent } from '../login-form/login-form.component';
+import { User } from '../../api/api.model';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatDialogModule,
+    LoginFormComponent,
+  ],
   templateUrl: './login.dialog.html',
   styleUrl: './login.dialog.scss'
 })
 export class LoginDialog {
 
+  dialogRef = inject(MatDialogRef)
+
+  onClose(user: User) {
+    this.dialogRef.close(user);
+  }
 }
