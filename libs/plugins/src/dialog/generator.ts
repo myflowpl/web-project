e4b547f16fc6f000adb25fbb8c6be9f1b8b6f37c
myflowpl <pedro.blaszczak@gmail.com>
2024-12-03 15:42:48 +0100

    dialog generator done

--- libs/plugins/src/dialog/generator.ts
+++ libs/plugins/src/dialog/generator.ts
@@ -1,24 +1,35 @@
 import {
-  addProjectConfiguration,
   formatFiles,
   generateFiles,
   Tree,
 } from '@nx/devkit';
 import * as path from 'path';
 import { DialogGeneratorSchema } from './schema';
-
+import { strings } from "@angular-devkit/core";
+ 
 export async function dialogGenerator(
   tree: Tree,
   options: DialogGeneratorSchema
 ) {
-  const projectRoot = `libs/${options.name}`;
-  addProjectConfiguration(tree, options.name, {
-    root: projectRoot,
-    projectType: 'library',
-    sourceRoot: `${projectRoot}/src`,
-    targets: {},
-  });
-  generateFiles(tree, path.join(__dirname, 'files'), projectRoot, options);
+
+  if(!options.path) {
+    options.path = '.';
+  }
+  
+  const projectRoot = `${options.path}/${options.name}`;
+
+  const vars = {
+    name: options.name,
+    dirName: options.name,
+    injectFnName: `inject${strings.classify(options.name)}Dialog`,
+    componentClassName: `${strings.classify(options.name)}Dialog`,
+    dataClassName: `${strings.classify(options.name)}DialogData`,
+    resultClassName: `${strings.classify(options.name)}DialogResponse`,
+
+  }
+
+  generateFiles(tree, path.join(__dirname, 'files'), projectRoot, vars);
+  
   await formatFiles(tree);
 }
 
--- libs/plugins/src/dialog/schema.d.ts
+++ libs/plugins/src/dialog/schema.d.ts
@@ -1,3 +1,4 @@
 export interface DialogGeneratorSchema {
   name: string;
+  path: string;
 }
