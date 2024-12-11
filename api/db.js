dab74784934d3f0989cd404184b3f053977d99a5
myflowpl <pedro.blaszczak@gmail.com>
2024-12-11 10:11:32 +0100

    migracja do nx

--- api/db.js
+++ api/db.js
@@ -1,9 +1,9 @@
 /* eslint-disable no-eval */
-const {existsSync, copyFileSync} = require('fs');
+const { existsSync, copyFileSync } = require('fs');
 
-if(!existsSync('./api/db.json')) {
+if (!existsSync('./api/db.json')) {
   copyFileSync('./api/db-tpl.json', './api/db.json');
   console.log('db data was initialized :)');
 } else {
-  console.log('db data already present :)')
+  console.log('db data already present :)');
 }
