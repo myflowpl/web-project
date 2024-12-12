c4e4fec41c8f1c0f2a8ee234cb702a8d3d6f0849
myflowpl <pedro.blaszczak@gmail.com>
2024-12-12 09:44:12 +0100

    loader helper for rxjs

--- libs/pet/src/lib/utils.ts
+++ libs/pet/src/lib/utils.ts
@@ -1,13 +1,31 @@
-import { pipe, tap } from "rxjs"
+import { DestroyRef, inject } from "@angular/core";
+import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
+import { catchError, EMPTY, pipe, tap } from "rxjs"
 
 export function injectLoader() {
 
+    const destroyRef = inject(DestroyRef);
+
     return {
         isLoading: false,
-        error: null,
-        tap() {
+        error: null as any,
+        tap<T>() {
+            
             return pipe(
-                
+                tap<T>({
+                    subscribe: () => {
+                        this.isLoading = true;
+                        this.error = null;
+                    },
+                    finalize: () => {
+                        this.isLoading = false;
+                    }
+                }),
+                catchError((error => {
+                    this.error = error;
+                    return EMPTY;
+                })),
+                takeUntilDestroyed(destroyRef),
             )
         }
     }
