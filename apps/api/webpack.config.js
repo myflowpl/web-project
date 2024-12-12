ca2e2847a797b24338c153a1f6dddc04863c0c2b
myflowpl <pedro.blaszczak@gmail.com>
2024-12-12 14:35:24 +0100

    swagger nx plugin for nest

--- apps/api/webpack.config.js
+++ apps/api/webpack.config.js
@@ -1,4 +1,4 @@
-const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
+const { NxAppWebpackPlugin, NxWebpackPlugin } = require('@nx/webpack/app-plugin');
 const { join } = require('path');
 
 module.exports = {
@@ -6,6 +6,16 @@ module.exports = {
     path: join(__dirname, '../../dist/apps/api'),
   },
   plugins: [
+    // new NxAppWebpackPlugin({
+    //   target: 'node',
+    //   compiler: 'tsc',
+    //   main: './src/main.ts',
+    //   tsConfig: './tsconfig.app.json',
+    //   assets: ['./src/assets'],
+    //   optimization: false,
+    //   outputHashing: 'none',
+    //   transformers: [{ name: '@nestjs/swagger/plugin' }], // <-- ADD the following line
+    // }),
     new NxAppWebpackPlugin({
       target: 'node',
       compiler: 'tsc',
