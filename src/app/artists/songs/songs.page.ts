078f241c20959e198df8769c21ccd6333dc16692
myflowpl <pedro.blaszczak@gmail.com>
2024-12-17 17:05:00 +0100

    pagination

--- src/app/artists/songs/songs.page.ts
+++ src/app/artists/songs/songs.page.ts
@@ -1,10 +1,16 @@
 import { Component, effect, inject } from '@angular/core';
 import { injectParamAsNumber } from '../../utils/signal.utils';
 import { SongsStore } from './songs.store';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatPaginatorModule} from '@angular/material/paginator';
 
 @Component({
   selector: 'app-songs',
-  imports: [],
+  imports: [
+    MatProgressBarModule,
+    MatPaginatorModule,
+
+  ],
   templateUrl: './songs.page.html',
   styleUrl: './songs.page.scss',
   providers: [ 
@@ -19,10 +25,12 @@ export class SongsPage {
 
   constructor() {
     
-    this.store.loadSongsByArtistId(this.artistId);
+    // this.store.loadSongsByArtistId(this.artistId);
 
-    // effect(() => {
-    //   this.store.loadSongsByArtistId(this.artistId());
-    // })
+    this.store.load(this.store.loadParams);
+
+    effect(() => {
+      this.store.setArtistId(this.artistId());
+    })
   }
 }
--- src/app/artists/songs/songs.store.ts
+++ src/app/artists/songs/songs.store.ts
@@ -1,33 +1,62 @@
-import { inject } from "@angular/core"
-import { Song } from "../../api/api.model"
-import { patchState, signalStore, withMethods, withState } from "@ngrx/signals"
+import { computed, inject } from "@angular/core"
+import { Song, SongDto } from "../../api/api.model"
+import { patchState, signalStore, withComputed, withMethods, withProps, withState } from "@ngrx/signals"
 import { rxMethod } from "@ngrx/signals/rxjs-interop"
 import { pipe, switchMap, tap } from "rxjs"
 import { SongsService } from "./songs.service"
+import { loaderSignal } from "../../utils/signal.utils"
 
 type SongsState = {
     songs: Song[],
     length: number,
     artistId: number,
+    pageSize: number,
+    pageIndex: number,
 }
 
 const initialState: SongsState = {
     songs: [],
     length: 0,
-    artistId: 0,
+    artistId: 1,
+    pageIndex: 0,
+    pageSize: 2,
 }
 
 export const SongsStore = signalStore(
     // {providedIn: 'root'},
     withState(initialState),
-
+    withComputed((store) => ({
+        loadParams: computed<SongDto>(() => ({
+            artistId: store.artistId(),
+            _page: store.pageIndex()+1,
+            _limit: store.pageSize()
+        }))
+    })),
+    withProps((store) => ({
+        loader: loaderSignal(),
+    })),
     withMethods((store, songsService = inject(SongsService)) => ({ 
+        setArtistId(artistId: number) {
+            patchState(store, {artistId});
+        },
+        // load artist songs
+        load: rxMethod<SongDto>(pipe(
+            tap(() => patchState(store, {songs: [], length: 0})),
+            switchMap(
+                (params) => songsService.getSongsByArtistId(params).pipe(
+                    store.loader.tap(),
+                    tap((res) => patchState(store, res)),
+                )
+            ),
+        )),
+
         // load artist songs
         loadSongsByArtistId: rxMethod<number>(pipe(
             tap(artistId => patchState(store, {artistId, songs: [], length: 0})),
             switchMap(
                 (artistId) => songsService.getSongsByArtistId({ artistId }).pipe(
-                    tap((res) => patchState(store, res))
+                    store.loader.tap(),
+                    tap((res) => patchState(store, res)),
                 )
             ),
         ))
