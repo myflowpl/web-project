ca374e0c20572e1bfaf5fa80975a884673afdb7b
myflowpl <pedro.blaszczak@gmail.com>
2024-12-18 16:59:13 +0100

    has role directive

--- src/app/auth/has-role.directive.ts
+++ src/app/auth/has-role.directive.ts
@@ -1,10 +1,28 @@
-import { Directive } from '@angular/core';
+import { computed, Directive, effect, inject, input, TemplateRef, ViewContainerRef } from '@angular/core';
+import { ProfileStore } from './profile.store';
 
 @Directive({
   selector: '[appHasRole]'
 })
 export class HasRoleDirective {
 
-  constructor() { }
+  appHasRole = input<string>()
+
+  containerRef = inject(ViewContainerRef);
+  templateRef = inject(TemplateRef);
+  profile = inject(ProfileStore);
+
+  // todo check role
+  flag = computed(() => !!this.profile.user())
+
+  constructor() { 
+    effect(() => {
+      if(this.flag()) {
+        this.containerRef.createEmbeddedView(this.templateRef);
+      } else {
+        this.containerRef.clear()
+      }
+    })
+  }
 
 }
--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,12 +1,14 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { ProfileStore } from '../../auth/profile.store';
+import { HasRoleDirective } from '../../auth/has-role.directive';
 
 @Component({
   selector: 'app-header',
   imports: [
     RouterLink,
     RouterLinkActive,
+    HasRoleDirective,
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
