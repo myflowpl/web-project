665c052ca7ef6dec902e150804da1ad64fc28b09
myflowpl <pedro.blaszczak@gmail.com>
2024-12-18 12:59:57 +0100

    login form

--- src/app/auth/login-form/login-form.component.ts
+++ src/app/auth/login-form/login-form.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { NonNullableFormBuilder, ReactiveFormsModule, UntypedFormBuilder } from '@angular/forms';
+import { ProfileStore } from '../profile.store';
 
 @Component({
   selector: 'app-login-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.scss'
 })
 export class LoginFormComponent {
 
+  store = inject(ProfileStore);
+
+  form = inject(UntypedFormBuilder).group({
+    email: ['piotr@myflow.pl'],
+    password: ['!@#$'],
+  })
+
+  handleSubmit() {
+    if(this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    this.store.login(this.form.value).subscribe(
+
+    );
+
+  }
 }
