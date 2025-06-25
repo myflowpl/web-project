9b1cc26182195f904b2e255d7cccb82d8a9f1ccd
myflowpl <pedro.blaszczak@gmail.com>
2025-06-25 15:53:55 +0200

    quote component

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,7 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { QuoteComponent } from "./quote/quote.component";
 import { AsyncPipe, JsonPipe } from '@angular/common';
-import { QuotesApi } from '@web/api-client';
+import { Quote, QuotesApi } from '@web/api-client';
 
 @Component({
   selector: 'app-home',
@@ -13,10 +13,15 @@ export class HomePage {
 
   quotesApi = inject(QuotesApi);
 
+  selected: Quote[] = [];
+
   quotes$ = this.quotesApi.quotesGet({
     pageSize: 3,
     pageIndex: 0,
 
   });
 
+  handleSelected(quote: Quote) {
+    this.selected.push(quote)
+  }
 }
--- src/app/home/quote/quote.component.ts
+++ src/app/home/quote/quote.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Quote } from '@web/api-client';
 
 @Component({
   selector: 'app-quote',
@@ -7,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrl: './quote.component.scss'
 })
 export class QuoteComponent {
+
+
+  @Input()
+  quote: Quote | null = null;
+
+  @Output()
+  selected = new EventEmitter<Quote>();
+
+  handleSelect(quote: Quote) {
+    this.selected.emit(quote);
+  }
 
 }
