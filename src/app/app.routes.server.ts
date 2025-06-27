a7dca0dde5980111ae5ecdf25f64eb00f7f1c870
myflowpl <pedro.blaszczak@gmail.com>
2025-06-27 10:14:30 +0200

    migrate to nx

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,23 +1,19 @@
 import { inject } from '@angular/core';
 import { RenderMode, ServerRoute } from '@angular/ssr';
 import { firstValueFrom } from 'rxjs';
-import { QuotesApi } from './api-client/lib';
+import { ArtistsApi } from './api-client/lib';
 
 export const serverRoutes: ServerRoute[] = [
-  // {
-  //   path: 'test',
-  //   renderMode: RenderMode.Prerender
-  // },
-  // {
-  //   path: 'test/:id',
-  //   renderMode: RenderMode.Prerender,
-  //   async getPrerenderParams() {
-  //     const quotesApi = inject(QuotesApi);
-  //     const quotes = await firstValueFrom(quotesApi.quotesGet());
+  {
+    path: 'artists/:artistId',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      const quotesApi = inject(ArtistsApi);
+      const artists = await firstValueFrom(quotesApi.artistsGet());
 
-  //     return quotes.data.map(quote => ({id: quote.id+''}))
-  //   },
-  // },
+      return artists.data.map(a => ({artistId: a.id+''}))
+    },
+  },
   {
     path: '**',
     renderMode: RenderMode.Prerender
