aa3dd28b58556ded19bff7a9e7f810440f3c8f2b
myflowpl <pedro.blaszczak@gmail.com>
2025-06-27 17:19:21 +0200

    admin resource page

--- src/app/admin/admin.page.ts
+++ src/app/admin/admin.page.ts
@@ -1,11 +1,12 @@
-import { Component, inject, resource } from '@angular/core';
+import { Component, effect, inject, model, resource, signal, viewChild } from '@angular/core';
 import { rxResource } from '@angular/core/rxjs-interop';
-import { MatPaginatorModule } from '@angular/material/paginator';
+import { FormsModule } from '@angular/forms';
+import { MatPaginator, MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 import { ArtistsApi, ArtistsGetRequestParams } from '@web/api-client';
 
 @Component({
   selector: 'app-admin',
-  imports: [MatPaginatorModule],
+  imports: [MatPaginatorModule, FormsModule],
   templateUrl: './admin.page.html',
   styleUrl: './admin.page.css',
 })
@@ -13,9 +14,26 @@ export class AdminPage {
 
   artistApi = inject(ArtistsApi);
 
+  q = model('');
+  pageIndex = model<number>(0);
+  pageSize = signal<number>(3);
+
   resource = rxResource({
-    params: (): ArtistsGetRequestParams => ({}),
+    params: (): ArtistsGetRequestParams => ({
+      pageIndex: this.pageIndex(),
+      pageSize: this.pageSize(),
+      q: this.q()
+    }),
     stream: (req) => this.artistApi.artistsGet(req.params)
   });
 
+  constructor() {
+    effect(() => {
+      console.log('m', this.q())
+    })
+  }
+
+  handlePage(page: PageEvent) {
+    this.pageIndex.set(page.pageIndex);
+  }
 }
